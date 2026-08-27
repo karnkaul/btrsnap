bc32f2aae5dffa9b952cbc058e6327d55d902a17
Karn Kaul <karnkaul@gmail.com>
2026-08-27 09:16:47 -0700

    chore: cleanup (#2)

--- lib/src/detail/shell.cpp
+++ lib/src/detail/shell.cpp
@@ -3,14 +3,8 @@
 #include "klib/cli/shell.hpp"
 
 namespace btrsnap::detail {
-namespace {
-constexpr auto elevate_v = std::string_view{"sudo"};
-} // namespace
-
 auto shell::execute(klib::CString const expression) -> bool {
 	log.debug("-- {}", expression);
 	return klib::shell::execute(expression) == klib::shell::success_v;
 }
-
-auto shell::execute_privileged(std::string_view const expression) -> bool { return execute(std::format("{} {}", elevate_v, expression)); }
 } // namespace btrsnap::detail
