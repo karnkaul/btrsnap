0e153b7516fc51d596bbddaefdf921b1bae25625
Karn Kaul <karnkaul@gmail.com>
2026-09-07 23:40:59 -0700

    WIP

--- tests/common/test_instance.hpp
+++ tests/common/test_instance.hpp
@@ -8,6 +8,12 @@ class TestInstance : public Instance {
   public:
 	explicit TestInstance() : Instance(std::make_unique<MockBtrfs>()) {}
 
+	[[nodiscard]] auto get_test_directory() const -> fs::path const& { return m_test_dir.get_path(); }
+
+	[[nodiscard]] auto create_config(std::string_view subvolume_subpath) const -> Config {
+		return Config{.subvolume = (get_test_directory() / subvolume_subpath).string()};
+	}
+
   private:
 	TestDirectory m_test_dir{};
 };
